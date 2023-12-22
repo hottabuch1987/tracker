7d8282fa7c3c5d50ae3e8c63ea5def5ca49ab731
hottabych <varvar1987a@mail.ru>
2023-12-22 18:55:08 +0300

    изменение цвета при помощи @click

--- ontrack/src/components/TheNav.vue
+++ ontrack/src/components/TheNav.vue
@@ -1,19 +1,30 @@
 <script setup>
+import { ref } from 'vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon} from '@heroicons/vue/24/outline'
+import NavItem from '@/components/NavItem.vue'
+
 const navItems = {
   время: ClockIcon, 
   активность: ListBulletIcon, 
   прогресс: ChartBarIcon,
 }
+
+const curentPage = ref('время')
+
 </script>
 <template>
     <nav class="sticky bottom-0 z-10 bg-white">
   <ul class="flex items-center justify-around border-t">
-    <li v-for="icon, page in navItems" :key="page" class="flex-1">
-      <a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
-        <component :is="icon" class="h-6 w-6" />{{ page }}
-      </a>
-    </li>
+    <NavItem 
+      v-for="icon, page in navItems" :key="page"
+      :href="`#${page}`"
+      :class="{'bg-gray-300 pointer-events-none': page === curentPage}"
+      @click="curentPage = page"
+    >
+
+      <component :is="icon" class="h-6 w-6" />{{ page }}
+    </NavItem>
+ 
   </ul>
 </nav>
 </template>
