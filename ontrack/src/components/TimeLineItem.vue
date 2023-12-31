ec6885d17178d76034dec8a0c39f939dcb2c981c
hottabych <varvar1987a@mail.ru>
2023-12-31 12:42:10 +0300

    выпадающий список 16

--- ontrack/src/components/TimeLineItem.vue
+++ ontrack/src/components/TimeLineItem.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { XMarkIcon } from '@heroicons/vue/24/outline'
+
 const props = defineProps(['timelineItem'])
 
 const hourLinkClasess = [
@@ -7,6 +9,13 @@ const hourLinkClasess = [
     ? 'bg-purple-900 font-black text-white'
     : 'bg-gray-100 text-gray-500'
 ]
+
+const options = [
+    {value: 1, label: 'Coding'},
+    {value: 2, label: 'Reading'},
+    {value: 3, label: 'Training'},
+
+]
 </script>
 
 <template>
@@ -14,6 +23,17 @@ const hourLinkClasess = [
     <a href="#" :class="hourLinkClasess">
         {{ timelineItem.hour }}:00
     </a>
+    <div class="flex gap-2">
+        <button class="rounded bg-gray-100 p-3 enabled:hover:bg-gray-200 disabled:cursor-not-allowed disabled:opasity-50">
+            <XMarkIcon class="h-8" />
+        </button>
+        <select name="" id="" class="w-full truncate rounded bg-gray-100 py-1 px-2 text-2xl">
+            <option selected disabled value="">Rest</option>
+            <option v-for="{value, label} in options" :key="value" :value="value">
+                {{ label }}
+            </option>
+        </select>
+    </div>
     
 </li>
         
