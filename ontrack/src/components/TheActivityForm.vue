885681744354c66189ee340a97bfa5700eb99558
hottabych <varvar1987a@mail.ru>
2024-01-08 11:05:00 +0300

    36 v-model активная кнопка если нет проблелов с очищением

--- ontrack/src/components/TheActivityForm.vue
+++ ontrack/src/components/TheActivityForm.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import { PlusIcon} from '@heroicons/vue/24/outline'
 import { isActivityValid } from '../validators';
 import BaseButton from './BaseButton.vue';
@@ -9,21 +10,22 @@ const emit = defineEmits({
     submit: isActivityValid
 })
 
-let activity = ''
+const activity = ref('')
 
 function submit() {
-    emit('submit', activity)
+    emit('submit', activity.value)
+    activity.value = ''
 }
 </script>
 <template>
     <form @submit.prevent="submit" class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4">
             <input type="text" 
-                :value="activity" 
-                @input="activity = $event.target.value" 
+
+                v-model="activity" 
                 class="w-full rounded border px-4 text-xl" 
                 placeholder="Активность"
             >
-            <BaseButton>
+            <BaseButton :disabled="activity.trim() === ''">
                 <PlusIcon class="h-8"/>
             </BaseButton>
     </form>      
