d6fc0607e3c2125952dc719ef613c0f74380954a
hottabych <varvar1987a@mail.ru>
2024-01-09 10:48:27 +0300

    38 компонент пустого списка

---
+++ ontrack/src/components/TheActivitesEmptyState.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col items-center gap-4 p-8 text-center grow">
+    <img src="../assets/images/not.svg" alt="not activity" class="h-48">
+    <span class="text-gray-400">Список пустой</span>
+  </div>    
+
+</template>
--- ontrack/src/pages/TheActivites.vue
+++ ontrack/src/pages/TheActivites.vue
@@ -3,7 +3,7 @@
 import TheActivityForm from '@/components/TheActivityForm.vue';
 import ActivityItem from '../components/ActivityItem.vue';
 import { isActivityValid, validateActivities } from '../validators'
-
+import TheActivitesEmptyState from '../components/TheActivitesEmptyState.vue'
 
 defineProps({
     activities: {
@@ -21,14 +21,16 @@ const emit = defineEmits({
 </script>
 
 <template>
-    <div>
-        <ul class="divide-y">
+    <div class="flex flex-col grow">
+        <ul v-if="activities.length" class="divide-y grow">
             <ActivityItem v-for="activity in activities" 
                 :key="activity" 
                 :activity="activity" 
                 @delete="emit('deleteActivity', activity)"
             />
         </ul>
+
+        <TheActivitesEmptyState v-else/>
         <TheActivityForm  @submit="emit('createActivity', $event)"/>
 
     </div>
